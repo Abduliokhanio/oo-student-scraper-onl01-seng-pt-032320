1055cd231ddb41b4cac90be4425bfbe6e8f651a3
Abdul Khan <akhan17@outlook.com>
2020-04-26 22:40:07 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,20 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+    binding.pry
+    doc.css(".student-name")[0].text       #name
+    doc.css(".student-location")[0].text   #location
+    doc.css(".student-card")[0].elements[0].attribute("href").value  #profile url
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
