b5ef796f3f089b95ed504547a00d757ca559ab41
Abdul Khan <akhan17@outlook.com>
2020-04-27 00:37:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,7 +25,7 @@ class Student
 
   def add_student_attributes(attributes_hash)
       attributes_hash.each do |key,value|
-        self.send(&quot;"#{key}"=&quot;, value)
+        self.send(&quot "#{key}"=&quot , value)
       end
   end
 
